f7e3d5c52f02bfaff52b308474d92ed388291f32
hamzanisar929 <tghamzaff@gmail.com>
2025-01-17 19:25:34 +0500

    1- Added userController validators    2- RecoveryRequest status changed to enum  3- Common api for active and non active user

--- src/database/migrations/20241221164951-recovery_requests.js
+++ src/database/migrations/20241221164951-recovery_requests.js
@@ -1,33 +1,37 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.createTable( 'recovery_requests' , {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable("recovery_requests", {
       id: {
         type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,
       },
-      user_id : {
-        type : Sequelize.DataTypes.INTEGER,
+      user_id: {
+        type: Sequelize.DataTypes.INTEGER,
         allowNull: false,
         references: {
-          model : 'users',
-          key: 'id'
+          model: "users",
+          key: "id",
         },
-        onUpdate : 'CASCADE',
-        onDelete: 'CASCADE'
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       status: {
-        type : Sequelize.DataTypes.INTEGER,
-        allowNull: false
-      }
-    })
+        type: Sequelize.DataTypes.ENUM("0", "1", "2"),
+        allowNull: false,
+        defaultValue: "0",
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.dropTable('recovery_requests');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("recovery_requests");
+    await queryInterface.sequelize.query(
+      'DROP TYPE IF EXISTS "enum_recovery_requests_status";'
+    );
+  },
 };
