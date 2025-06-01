ee1cb10d2bc2f5e8a1a7ad47a9325f2bcb79c9ef
Nouman Riaz <mriaz@agholding.net>
2025-06-01 14:32:48 +0500

    seeder updated

--- src/database/seeders/20250123155522-user_seeder.js
+++ src/database/seeders/20250123155522-user_seeder.js
@@ -6,48 +6,46 @@ const { UserRole , Role , User } = require('../models');
 module.exports = {
 
   async up (queryInterface, Sequelize) {
-    // let saltcount  = 10;
+    let saltcount  = 10;
 
-    // let activeUsers = [
-    //   {name : 'Nouman Riaz' , number : '3453453455' , email : 'mnoumanb@gmail.com' , password : 'nouman123' , status : 1 },
-    //   {name : 'Usman Alam' , number : '2342342343' , email : 'usman@gmail.com' , password : 'usman123' , status : 1 },
-    //   {name : 'Arsalan Khattak' , number : '56786746565' , email : 'arsalan@gmail.com' , password : 'arsalan123' , status : 1 },
-    // ];
+    let activeUsers = [
+      {name : 'Nouman Riaz' , number : '3453453455' , email : 'mnoumanb@gmail.com' , password : 'nouman123' , status : 1 },
+      {name : 'Usman Alam' , number : '2342342343' , email : 'usman@gmail.com' , password : 'usman123' , status : 1 },
+      {name : 'Arsalan Khattak' , number : '56786746565' , email : 'arsalan@gmail.com' , password : 'arsalan123' , status : 1 },
+    ];
 
    
-    // let nonActiveUsers = [
-    //   {name : 'Haris Rauf' , number : '5756756766' , email : 'haris@gmail.com' , password : 'haris123' , status : 0 },
-    //   {name : 'Basit' , number : '3453454454' , email : 'basit@gmail.com' , password : 'basit123' , status : 0 },
-    //   {name : 'Shakir khattak' , number : '34536754754' , email : 'shakir@gmail.com' , password : 'shakir123' , status : 0 },
-    // ];
+    let nonActiveUsers = [
+      {name : 'Haris Rauf' , number : '5756756766' , email : 'haris@gmail.com' , password : 'haris123' , status : 0 },
+      {name : 'Basit' , number : '3453454454' , email : 'basit@gmail.com' , password : 'basit123' , status : 0 },
+      {name : 'Shakir khattak' , number : '34536754754' , email : 'shakir@gmail.com' , password : 'shakir123' , status : 0 },
+    ];
 
-    // async function processUsers() {
-    //   activeUsers = await Promise.all(
-    //     activeUsers.map(async (user) => {
-    //       const mutatedPassword = await bcrypt.hash(user.password, saltcount);
-    //       return { ...user, password: mutatedPassword };
-    //     })
-    //   );
+    async function processUsers() {
+      activeUsers = await Promise.all(
+        activeUsers.map(async (user) => {
+          const mutatedPassword = await bcrypt.hash(user.password, saltcount);
+          return { ...user, password: mutatedPassword };
+        })
+      );
     
-    //   nonActiveUsers = await Promise.all(
-    //     nonActiveUsers.map(async (user) => {
-    //       const mutatedPassword = await bcrypt.hash(user.password, saltcount);
-    //       return { ...user, password: mutatedPassword };
-    //     })
-    //   );
+      nonActiveUsers = await Promise.all(
+        nonActiveUsers.map(async (user) => {
+          const mutatedPassword = await bcrypt.hash(user.password, saltcount);
+          return { ...user, password: mutatedPassword };
+        })
+      );
     
-    //   const userList = [...activeUsers, ...nonActiveUsers];
+      const userList = [...activeUsers, ...nonActiveUsers];
     
-    //  return userList;
-    // }
+     return userList;
+    }
 
 
     
-    // let users = await processUsers();
+    let users = await processUsers();
 
-    // await queryInterface.bulkInsert('users', users);
-
-
+    await queryInterface.bulkInsert('users', users);
 
     let roles = await Role.findAll();
     let activeStatusUser = await User.findAll({ where: {status : 1 }});
