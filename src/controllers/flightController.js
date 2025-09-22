c0eaed561e09f56762debd7591f80a56ec2271f9
Nouman Riaz <mriaz@agholding.net>
2025-09-22 18:57:05 +0500

    invoice uncomment

--- src/controllers/flightController.js
+++ src/controllers/flightController.js
@@ -2268,10 +2268,10 @@ createBooking: async (request, response) => {
 
     if( result && result.confirmationId) {
 
-      return response.status(200).json({
-                    status: true,
-                     data : result,
-                });
+      // return response.status(200).json({
+      //               status: true,
+      //                data : result,
+      //           });
 
       const PNR = result.confirmationId;
       const fareInfo = result.booking.fares[0]?.totals || {};
@@ -2375,19 +2375,19 @@ createBooking: async (request, response) => {
         flights: flightsDetail,
       };
       const html = await ejs.renderFile(invoiceTemplate, pdfData);
-      const browser = await puppeteer.launch();
-      // const browser = await puppeteer.launch({
-      //                                   headless: true,
-      //                                   args: [
-      //                                     '--no-sandbox',
-      //                                     '--disable-setuid-sandbox',
-      //                                     '--disable-dev-shm-usage',
-      //                                     '--disable-accelerated-2d-canvas',
-      //                                     '--no-zygote',
-      //                                     '--single-process',
-      //                                     '--disable-gpu'
-      //                                   ]
-      //                                 });
+      //const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+                                        headless: true,
+                                        args: [
+                                          '--no-sandbox',
+                                          '--disable-setuid-sandbox',
+                                          '--disable-dev-shm-usage',
+                                          '--disable-accelerated-2d-canvas',
+                                          '--no-zygote',
+                                          '--single-process',
+                                          '--disable-gpu'
+                                        ]
+                                      });
       const page = await browser.newPage();
       await page.setContent(html, { waitUntil: "load" });
       const fileName = `${moment().unix()}-${request.user.name}-${PNR}.pdf`;
@@ -2462,9 +2462,16 @@ cancelBooking : async (request, response) =>{
   const payload = {
       "confirmationId": "TOIOYG",
       "cancelAll": true,
-      "offerItemId": "21",
-      "retrieveBooking": true
-  }
+      // "offerItemId": "21",
+      "retrieveBooking": true,
+      "segments": [
+        {
+          "sequence": 1,
+          "id": "21"
+        }
+     ],
+    "targetPcc": "3GML",
+    }
 
     const requestOptions = {
       method: "POST",
