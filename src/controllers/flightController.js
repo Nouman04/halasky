7a67c7dc12b86515d21f71ffebf00bfb92995222
Nouman Riaz <mriaz@agholding.net>
2025-11-27 21:33:58 +0500

    invoice issue

--- src/controllers/flightController.js
+++ src/controllers/flightController.js
@@ -2404,7 +2404,19 @@ const pdfData = {
 };
 
 const html = await ejs.renderFile(invoiceTemplate, pdfData);
-const browser = await puppeteer.launch();
+// const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+                                        headless: true,
+                                        args: [
+                                          '--no-sandbox',
+                                          '--disable-setuid-sandbox',
+                                          '--disable-dev-shm-usage',
+                                          '--disable-accelerated-2d-canvas',
+                                          '--no-zygote',
+                                          '--single-process',
+                                          '--disable-gpu'
+                                        ]
+                                      });
 const page = await browser.newPage();
 
 await page.setContent(html, { waitUntil: "load" });
