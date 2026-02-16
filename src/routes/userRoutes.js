47f9ce77dfaed50be293429ea9fbb142c7a730ee
Nouman Riaz <mriaz@agholding.net>
2026-02-16 12:30:10 +0500

    controller spelling issue

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/UserController');
+const UserController = require('../controllers/userController');
 const passport = require('passport');
 
 router.use(passport.authenticate('jwt', { session: false }));
